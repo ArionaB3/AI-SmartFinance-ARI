7a25ad8ec1104f492ff4f1d45368e6d2e73b51c5
ArionaB3 <arionaberisha4@gmail.com>
2025-02-04 23:39:48 +0100

    Initial commit

---
+++ drizzle.config.js
@@ -0,0 +1,10 @@
+export default {
+  dialect: "postgresql",
+  schema: "./utils/schema.jsx",
+  out: "./drizzle",
+  dbCredentials: {
+    url: "postgresql://neondb_owner:npg_JE9ubArPYD8Z@ep-winter-morning-a83gr8uk-pooler.eastus2.azure.neon.tech/neondb?sslmode=require",
+    connectionString:
+      "postgresql://neondb_owner:npg_JE9ubArPYD8Z@ep-winter-morning-a83gr8uk-pooler.eastus2.azure.neon.tech/neondb?sslmode=require",
+  },
+};
